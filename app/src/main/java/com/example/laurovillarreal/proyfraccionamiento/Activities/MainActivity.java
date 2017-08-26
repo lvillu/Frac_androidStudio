809b5ed5c8d1eb0b5d35305dfbc5aae3d1a116ce
laurovillarreal <lauro.villarreallucio@gmail.com>
2017-08-26 16:20:03 -0500

    Cambios con carpetas

--- app/src/main/java/com/example/laurovillarreal/proyfraccionamiento/Activities/MainActivity.java
+++ app/src/main/java/com/example/laurovillarreal/proyfraccionamiento/Activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.laurovillarreal.proyfraccionamiento.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.laurovillarreal.proyfraccionamiento.R;
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this.getBaseContext(), "Hola mundo desde toas.", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/laurovillarreal/proyfraccionamiento/Class/Base.java
@@ -0,0 +1,8 @@
+package com.example.laurovillarreal.proyfraccionamiento.Class;
+
+/**
+ * Created by laurovillarreal on 26/08/17.
+ */
+
+public class Base {
+}
